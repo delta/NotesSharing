2f2f524b3c56cd38462a1d5f6d434cc3c685b2e1
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-03-17 20:25:39 +0530

    Refactor done.

--- app/__init__.py
+++ app/__init__.py
@@ -7,5 +7,5 @@ app = Flask(__name__)  # Our Flask instance
 app.config.from_object('config')  # Setting our WTF config
 db = SQLAlchemy(app)
 
-from controllers import views
+from controllers import routes
 import models
