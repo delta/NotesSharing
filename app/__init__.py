7d4cac4d243fab90352bf091dc8b0567a49339a4
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-03-19 02:41:36 +0530

    Fix for accidental erase.

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,11 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+# We also need to initialize our database
 
+
+app = Flask(__name__)  # Our Flask instance
+app.config.from_object('config')  # Setting our WTF config
+db = SQLAlchemy(app)
+
+from controllers import routes
+import models
