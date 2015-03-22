bb3ed8475e1978d7fae8565022ff825645ce2eab
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-03-22 23:50:09 +0530

    Login form.

--- app/controllers/forms.py
+++ app/controllers/forms.py
@@ -1,10 +1,11 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import StringField, BooleanField, PasswordField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(Form):
     # DataRequired checks if the field is not left empty
-    department = StringField('Department', validators=[DataRequired()])
+    rollnumber = StringField('Roll Number', validators=[DataRequired()])
+    password = PasswordField('New Password', validators=[DataRequired()])
     # If you have validators attached , they have errors added under form.fieldname.erros
     # remember_me = BooleanField('remember me', default = False)
