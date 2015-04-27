7fe27189e8b33fd9a0e698a63afc4ebf5e41b07b
nirvik <nirvik1993@gmail.com>
2015-04-27 11:52:26 +0530

    Added odt format to settings

--- app/settings.py
+++ app/settings.py
@@ -4,7 +4,7 @@ class Config(object):
     basedir = os.path.abspath(os.path.dirname(__file__))
     WTF_CSRF_ENABLED = True
     UPLOAD_FOLDER = basedir + '/../tmp'
-    ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','odp','pptx','docx','doc','ppt','rtf'])
+    ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','odp','pptx','docx','doc','ppt','rtf','odt'])
     IMAP_SERVER = 'webmail.nitt.edu'
     IMAP_SERVER_PORT = '143'
     WHOOSH_BASE = os.path.join(basedir, 'search.db') #A special database that is dedicated to full text search
