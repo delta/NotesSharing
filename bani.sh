cab77b0fd3dead1f45ed47d13b8c5dc7a8183c81
nirvik <nirvik1993@gmail.com>
2015-05-02 16:10:03 +0530

    Make some changes that will be undone

--- bani.sh
+++
@@ -1,1 +0,0 @@
-curl "http://0.0.0.0:8983/solr/update?stream.body=%3Cdelete%3E%3Cquery%3E*:*%3C/query%3E%3C/delete%3E&commit=true"
--- index.py
+++
@@ -1,8 +0,0 @@
-import urllib
-import pycurl
-import requests
-c= pycurl.Curl()
-requests.post("http://localhost:8983/solr/firenotes/update/extract?literal.id=Ch6_STLAssociativeContainersAndIterators.pdf&commit=true -F myfile=@/home/nirvik/Dev/code/FLASK_APPS/fireNotes/app/controllers/../../tmp/Ch6_STLAssociativeContainersAndIterators.pdf")
-#c.setopt(c.URL,s)
-#c.perform()
-print c
